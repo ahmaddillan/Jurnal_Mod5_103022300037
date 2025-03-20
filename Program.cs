896fd67641bb5f142fc6415e210a77acc521f05d
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-20 10:58:12 +0700

    test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class pemrosesData() { 
+    public T DapatkanNilaiTerbesar<T>(T nil, T lai, T a) {
+        dynamic b = nil;
+        dynamic c = lai;
+        dynamic d = a;
+        dynamic terbesar = b;
+        if (Convert.ToInt32(terbesar) < Convert.ToInt32(c))
+        {
+            terbesar = c;
+        }
+        if (Convert.ToInt32(terbesar) < Convert.ToInt32(d))
+        {
+            terbesar = d;
+        }
+
+        return terbesar;
+    }
+}
+
+public class SimpleDataBase() { 
+    private List<int> storedData = new List<int>();
+    private List<DateTime> inputDates = new List<DateTime>();
+
+    public void simpleDatabase() { 
+        
+    }
+
+    public void addNewData() { 
+    
+    }
+
+}
+
+
+public class program() {
+    public static void Main() {
+        String in1, in2, in3;
+        pemrosesData proses = new pemrosesData();
+
+        Console.WriteLine("masukkan nilai pertama: ");
+        in1 = Console.ReadLine();
+        Console.WriteLine("masukkan nilai kedua: ");
+        in2 = Console.ReadLine();
+        Console.WriteLine("masukkan nilai ketiga: ");
+        in3 = Console.ReadLine();
+
+       Console.WriteLine("nilai terbesar: " + proses.DapatkanNilaiTerbesar(in1,in2,in3));
+    }
+}
